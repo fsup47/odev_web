a3b33c03cdedc2f104c85693d4a3000fde9a75ef
FiratSaüBilgMüh <FiratSaBilgMh@DESKTOP-QT9T7U7>
2022-08-12 14:02:57 +0300

    deneme

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,31 +7,33 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace odev_web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public ViewResult  AllCreatures()
         {
+
+            return View();
+        }
+     
+        public ViewResult Contact()
+        {
+
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
+
 }
--- README.md
+++ README.md
@@ -6,10 +6,17 @@ b181210039
 Sakarya Üniversitesi Bilgisayar mühendisliği öğrencisi
 
 Açıklama
+Nesli tükenmek üzere olan hayvanlar, endemik bitkilerin listelendiği,
+her türün altında yorum yapabilen ve sadece adminin tür ekleyip düzenleyebildiği bir websitesi yapmak.
+
+Tarihe Göre yaptığım işlemler
+03.08.2022 Githubı kullanmaya başladım.
+08.08.2022 Yapacaklarımı yaptıklarımı anlattığım metin dosyasını oluşturdum.
+12.08.2022 
 yapılmak istenen konu açıkla
 
   Projede kullanılanlar (Visual Studio 2019,IDE)
-->	Asp.net Core 5 MVC 
+->	Asp.net Core 3,1 MVC 
 ->	C#
 ->	Veritabanı olarak SQL Server /PostgreSQL/ vb
 ->	Entity Framework Core ORM
